a21ed5232bd6a13ed9afee518e7465064fdd6dfb
marina <marina_bakum@mail.ru>
2018-03-08 14:22:36 +0200

    standard

--- shopList/todo.js
+++ shopList/todo.js
@@ -5,46 +5,46 @@ const Todo = require('./model')
 const router = express.Router()
 
 //get all todo
-router.get('/todo', (req, res, next) => {
-  Todo.find({})
-    .then(todo => {
-      res.json({todo})
-    })
-    .catch(next)
+router.get('/todo', function (req, res, next) {
+    Todo.find({})
+        .then(function (todo) {
+            res.json({todo})
+        })
+        .catch(next)
 })
 
 //get todo by id
-router.get('/todo/:id', (req, res) => {
-    Todo.findById({'_id': req.params.id}, (err, todo) => {
-      if (todo === null) {
-          //if todo not exist error 404
-          res.status(404).json("Not found")
-      } else {
-          res.status(200).json(todo)
-      }
+router.get('/todo/:id', function (req, res) {
+    Todo.findById({'_id': req.params.id}, function (err, todo) {
+        if (todo === null) {
+            //if todo not exist error 404
+            res.status(404).json("Not found")
+        } else {
+            res.status(200).json(todo)
+        }
     })
 })
 
 //delete todo by id
 router.delete('/todo/:id', function (req, res) {
-  let id = req.params.id
-  Todo.remove({
-    _id: id
-  }, function () {
-      //if delete successful
-      res.json('Todo deleted')
-  })
+    let id = req.params.id
+    Todo.remove({
+        _id: id
+    }, function () {
+        //if delete successful
+        res.json('Todo deleted')
+    })
 })
 
 //post new todo
-router.post('/todo', (req, res, next) => {
+router.post('/todo', function (req, res, next) {
     let newTodo = new Todo(req.body.todo)
     //check if field text not empty and exist
-    if (req.body.todo.text !== '' && req.body.todo.text){
+    if (req.body.todo.text !== '' && req.body.todo.text) {
         newTodo.save()
-        .then(todo => {
-            res.json({todo})
-        })
+            .then(function (todo) {
+                res.json({todo})
+            })
     } else {
         res.json({success: false, message: 'Field text is empty!'})
     }
@@ -54,9 +54,9 @@ router.post('/todo', (req, res, next) => {
 router.put('/todo/:id', function (req, res) {
     // if text is empty then text = defaultText
     let defaultText = "text"
-    Todo.findOneAndUpdate({ "_id": req.params.id}, {
+    Todo.findOneAndUpdate({"_id": req.params.id}, {
         text: req.body.todo.text || defaultText
-    }, { new: true }, function (err, doc) {
+    }, {new: true}, function (err, doc) {
         if (err) {
             res.status(400).json(err)
         }
